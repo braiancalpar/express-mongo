8b59516219540cda57e64f33e56ec0062e867092
braiancalpar <braian.santos@calpar.com.br>
2026-09-08 09:37:53 -0300

    feat: adiciona método para listar livros por editora e atualiza rotas

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -65,6 +65,16 @@ class LivroController {
       res.status(500).json({ message: `${erro.message} - falha na exclusão` });
     }
   }
+
+  static async listarLivrosPorEditora(req, res) {
+    const editora = req.query.editora;
+    try {
+      const livrosPorEditora = await livro.find({ editora: editora });
+      res.status(200).json(livrosPorEditora);
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na busca` });
+    }
+  }
 }
 
 export default LivroController;
