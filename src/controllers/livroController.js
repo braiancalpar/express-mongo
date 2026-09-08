c10869385827061afd14f397061a9c7ab5659121
braiancalpar <braian.santos@calpar.com.br>
2026-09-08 08:53:21 -0300

    feat: adiciona método para excluir livro e atualiza rotas

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -39,11 +39,21 @@ class LivroController {
     try {
       const id = req.params.id;
       await livro.findByIdAndUpdate(id, req.body);
-      res.status(200).json({ message: "Livro atualizado"});
+      res.status(200).json({ message: "Livro atualizado" });
     } catch (erro) {
       res
         .status(500)
         .json({ message: `${erro.message} - falha na atualização` });
+    }
+  }
+
+  static async excluirLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(200).json({ message: "Livro excluído com sucesso" });
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na exclusão` });
     }
   }
 }
