dcf36e60d2280688593b3eff15e92697120f4000
braiancalpar <braian.santos@calpar.com.br>
2026-09-08 09:09:43 -0300

    feat: adiciona controlador e rotas para gerenciamento de autores

---
+++ src/models/Autor.js
@@ -0,0 +1,14 @@
+import moongose from "mongoose";
+
+const autorSchema = new moongose.Schema(
+  {
+    id: { type: mongoose.Schema.Types.ObjectId },
+    nome: { type: String, required: true },
+    nacionalidade: { type: String },
+  },
+  { versionKey: false },
+);
+
+const autor = moongose.model("autores", autorSchema);
+
+export { autor, autorSchema };
